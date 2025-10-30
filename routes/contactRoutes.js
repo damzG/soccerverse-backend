5eebab185f5ec2b7540aa0449b9b6a15d5fc3cda
damzG <C00313475@setu.ie>
2025-10-30 14:26:30 +0000

    Initial backend commit

---
+++ routes/contactRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import Contact from "../models/Contact.js";
+
+const router = express.Router();
+router.post("/", async(req, res) => {
+    try{
+        const { name, email, message } = req.body;
+
+        //Simple validation
+        if(!name || !email || !message){
+            return res.status(400).json({success: false, error: "All fields are required"});
+        }
+        const newContact = new Contact({name, email, message});
+        await newContact.save();
+        console.log("✅ New contact saved:", newContact);
+        res.json({success: true, message: "Contact saved successfully"});
+    }catch(error){
+        console.error("❌ Error saving contact:", error);
+        res.status(500).json({error: "Failed to save contact"});
+    }
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import process from "process";
+import cors from "cors";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+//Middleware
+app.use(cors(
+    {
+    origin: "http://localhost:5173", // your Vite frontend
+    methods: ["GET", "POST"],
+    credentials: true, // optional, but useful if you later use cookies or auth
+  }
+));
+app.use(express.json());
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("✅ MongoDB connected")).catch((err) => {console.error("❌ MongoDB error:", err.message); console.log("Connection string (sanitized): ", process.env.MONGO_URI.replace(/:[^:@]+@/, ':****@'))});
+
+//Routes
+app.use("/api/contact", contactRoutes);
+
+app.get("/", (req, res) => {
+    res.send("SoccerVerse backend is running🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
